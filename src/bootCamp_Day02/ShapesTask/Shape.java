7b4e1531dd39ee315c416da977259d342b77eac2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-16 12:15:44 -0500

    day01

---
+++ src/bootCamp_Day02/ShapesTask/Shape.java
@@ -0,0 +1,17 @@
+package bootCamp_Day02.ShapesTask;
+
+public class Shape {
+
+    public double area;
+    public double perimeter;
+
+    public double calculateArea(){
+        return 0;
+    }
+
+    public double calculatePerimeter(){
+        return 0;
+    }
+
+
+}
