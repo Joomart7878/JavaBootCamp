ab781cdcde27d9a88671154a00927bc344bf1929
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-16 12:52:22 -0500

    day02

---
+++ src/bootCamp_Day02/ShapesTask/ShapeObjects.java
@@ -0,0 +1,21 @@
+package bootCamp_Day02.ShapesTask;
+
+public class ShapeObjects {
+
+    public static void main(String[] args) {
+        // r = 5;
+
+        Circle circle = new Circle(5);
+
+        System.out.println(circle);
+
+        // w = 15, l = 20
+
+        Rectangle rectangle = new Rectangle(15, 10);
+
+        System.out.println(rectangle);
+
+
+    }
+
+}
